849807a0e1549901232e3f92170fff35a6b32537
JustKilli <justkilli@outlook.de>
2023-09-17 11:51:18 +0200

    POM-3 Create default mysql config

--- src/main/java/net/justkilli/Main.java
+++ src/main/java/net/justkilli/Main.java
@@ -1,7 +1,28 @@
 package net.justkilli;
 
+import net.justkilli.killisessentials.config.handler.IConfigHandler;
+import net.justkilli.killisessentials.config.handler.YAMLConfigHandler;
+import net.justkilli.killisessentials.config.values.ConfigValue;
+
 public class Main {
+
+    private static final String CONFIG_FOLDER_NAME = "configs";
+    private static final String MYSQL_CONFIG_NAME = "mysql.yml";
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        createDefaultMysqlConfig(String.format("%s/%s", CONFIG_FOLDER_NAME, MYSQL_CONFIG_NAME));
     }
+
+    public static IConfigHandler createDefaultMysqlConfig(String path) {
+        IConfigHandler mysqlConfigHandler = new YAMLConfigHandler(path);
+        mysqlConfigHandler.addDefaultValue(ConfigValue.SQL_HOST);
+        mysqlConfigHandler.addDefaultValue(ConfigValue.SQL_DATABASE);
+        mysqlConfigHandler.addDefaultValue(ConfigValue.SQL_PORT);
+        mysqlConfigHandler.addDefaultValue(ConfigValue.SQL_USERNAME);
+        mysqlConfigHandler.addDefaultValue(ConfigValue.SQL_PASSWORD);
+        mysqlConfigHandler.save();
+        return mysqlConfigHandler;
+    }
+
 }
